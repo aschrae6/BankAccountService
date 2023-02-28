360629f695995d2e392e86ed84f1876a3f017c70
aschrae6 <adam.schraedel@ey.com>
2023-02-28 12:03:47 -0500

    package structure

--- src/main/java/com/monopolycapstone/AccountServiceApplication.java
+++ src/main/java/com/monopolycapstone/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.monopolycapstone.accountservice;
+package com.monopolycapstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
